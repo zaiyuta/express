b7e316dbd24f89834b007e0694787236f4430810
zaiyuta <zaima2basehit@gmail.com>
2026-06-06 23:26:57 +0900

    fix app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
+var dogRouter = require('./routes/dog');
 
 var app = express();
 
@@ -27,6 +28,8 @@ app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
 app.use('/cat', catRouter);
+app.use('/dog', dogRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
